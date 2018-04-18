4eadd4f83a85daf5c03876ca76df914fc152182c
JHenshall4 <jimmyh543@hotmail.com>
2018-04-18 19:27:30 +1000

    imported temp brain model

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -125,7 +125,7 @@ public class Interactable : MonoBehaviour {
                 GameObject.Find("Eye 1").transform.localPosition = new Vector3(-0.02f, 0.18f, 1.71f);
                 GameObject.Find("Eye 2").transform.localPosition = new Vector3(-0.02f, 0.18f, 3.71f);
                 GameObject.Find("Mouth").transform.localPosition = new Vector3(-0.04f, 0.63f, 0.07f);
-                GameObject BrainBit = (GameObject)Instantiate(Resources.Load("Sphere"));
+                GameObject BrainBit = (GameObject)Instantiate(Resources.Load("brain_prefab"));
                 BrainBit.transform.position = new Vector3(0.12f, 2.01f, 2.01f);
                 Destroy(GetComponent<Interactable>());
             }
